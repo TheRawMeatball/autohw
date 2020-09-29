e356fa38f0906ed74e4b456340b03c80c349fce2
TheRawMeatball <therawmeatball@gmail.com>
2020-09-29 23:55:20 +0300

    Added favicon

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ extern crate chrono;
 extern crate num_traits;
 
 use diesel::prelude::*;
+use rocket::fairing::AdHoc;
+use rocket_contrib::serve::StaticFiles;
 
 mod actions;
 mod auth_guard;
@@ -29,5 +31,10 @@ fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .attach(DbConn::fairing())
         .attach(handlebars_helpers::helpers())
+        .attach(AdHoc::on_attach("Static Files config", |mut rocket| async {
+            let files_path = rocket.config().await.get_string("static_files_path")
+                .expect("missing static files path!");
+            Ok(rocket.mount("/static", StaticFiles::from(files_path)))
+        }))
         .mount("/", endpoints::routes())
 }
